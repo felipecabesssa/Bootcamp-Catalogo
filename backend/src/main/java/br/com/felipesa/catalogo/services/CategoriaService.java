12141128adec13e856c70174d289f499ea95602e
felipecabesssa <felipesoares100@hotmail.com>
2022-07-19 14:27:22 -0300

    Seeding do Banco de dados

--- backend/src/main/java/br/com/felipesa/catalogo/services/CategoriaService.java
+++ backend/src/main/java/br/com/felipesa/catalogo/services/CategoriaService.java
@@ -2,8 +2,10 @@ package br.com.felipesa.catalogo.services;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.felipesa.catalogo.entidades.Categoria;
 import br.com.felipesa.catalogo.repositories.CategoriaRepository;
@@ -14,6 +16,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<Categoria> findAll(){
 		
 		return repository.findAll();
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_categoria(nome) VALUES ('Livros');
+INSERT INTO tb_categoria(nome) VALUES ('Eletronicos');
+INSERT INTO tb_categoria(nome) VALUES ('Computadores');
