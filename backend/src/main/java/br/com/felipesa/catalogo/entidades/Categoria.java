614552bed273d39fa246fe9aef55f60a9bead497
felipecabesssa <felipesoares100@hotmail.com>
2022-08-12 12:02:07 -0300

    Auditoria Categoria - Instant - Hora ao Salvar e Atualizar

--- backend/src/main/java/br/com/felipesa/catalogo/entidades/Categoria.java
+++ backend/src/main/java/br/com/felipesa/catalogo/entidades/Categoria.java
@@ -1,12 +1,16 @@
 package br.com.felipesa.catalogo.entidades;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +22,12 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant criadoAs;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant atualizadoAs;	
 	
 	public Categoria() {
 	}
@@ -41,6 +51,24 @@ public class Categoria implements Serializable{
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	public Instant getCriadoAs() {
+		return criadoAs;
+	}
+
+	public Instant getAtualizadoAs() {
+		return atualizadoAs;
+	}
+	
+	@PrePersist
+	public void antesDePersistir() {
+		criadoAs = Instant.now();
+	}
+	
+	@PreUpdate
+	public void antesDeAtualizar() {
+		atualizadoAs = Instant.now();
 	}
 
 	@Override
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_categoria(nome) VALUES ('Livros');
-INSERT INTO tb_categoria(nome) VALUES ('Eletronicos');
-INSERT INTO tb_categoria(nome) VALUES ('Computadores');
+INSERT INTO tb_categoria(nome, criado_As) VALUES ('Livros', NOW());
+INSERT INTO tb_categoria(nome, criado_As) VALUES ('Eletronicos', NOW());
+INSERT INTO tb_categoria(nome, criado_As) VALUES ('Computadores', NOW());
