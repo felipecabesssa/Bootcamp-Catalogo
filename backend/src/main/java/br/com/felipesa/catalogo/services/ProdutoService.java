9894e3cde1927d6c192a39cfaa2c2f7781296ec8
felipecabesssa <felipesoares100@hotmail.com>
2022-08-22 23:48:35 -0300

    Produto - Inserir, Alterar e Del - DTO + Entidade

--- backend/src/main/java/br/com/felipesa/catalogo/services/ProdutoService.java
+++ backend/src/main/java/br/com/felipesa/catalogo/services/ProdutoService.java
@@ -1,7 +1,6 @@
 package br.com.felipesa.catalogo.services;
 
 import java.util.Optional;
-import java.util.Set;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -13,9 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.felipesa.catalogo.dto.CategoriaDTO;
 import br.com.felipesa.catalogo.dto.ProdutoDTO;
 import br.com.felipesa.catalogo.entidades.Categoria;
 import br.com.felipesa.catalogo.entidades.Produto;
+import br.com.felipesa.catalogo.repositories.CategoriaRepository;
 import br.com.felipesa.catalogo.repositories.ProdutoRepository;
 import br.com.felipesa.catalogo.services.exceptions.EntidadeNaoEncontradaException;
 import br.com.felipesa.catalogo.services.exceptions.IntegridadeReferencialException;
@@ -25,6 +26,9 @@ public class ProdutoService {
 	
 	@Autowired
 	private ProdutoRepository repositorio;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepositorio;
 
 	@Transactional(readOnly = true)
 	public Page<ProdutoDTO> buscaTodasServicePaginado(PageRequest pageRequest){		
@@ -42,7 +46,7 @@ public class ProdutoService {
 	@Transactional
 	public ProdutoDTO insereProdutoService(ProdutoDTO dto) {
 		Produto entidade = new Produto();
-		//entidade.setNome(dto.getNome());
+		copiaDtoParaEntidade(dto, entidade);
 		entidade = repositorio.save(entidade);
 		
 		return new ProdutoDTO(entidade);
@@ -52,7 +56,7 @@ public class ProdutoService {
 	public ProdutoDTO alteraProdutoService(Long id, ProdutoDTO dto) {
 		try {
 			Produto entidade = repositorio.getReferenceById(id);
-			//entidade.setNome(dto.getNome());
+			copiaDtoParaEntidade(dto, entidade);
 			entidade = repositorio.save(entidade);
 			return new ProdutoDTO(entidade);
 		}
@@ -71,6 +75,21 @@ public class ProdutoService {
 		catch (DataIntegrityViolationException e) {
 			throw new IntegridadeReferencialException("Violação de Integridade, a categoria tem produtos associados");
 		}
+	}
+	
+	private void copiaDtoParaEntidade(ProdutoDTO dto, Produto entidade) {
+		entidade.setNome(dto.getNome());
+		entidade.setDescricao(dto.getDescricao());
+		entidade.setData(dto.getData());
+		entidade.setImgUrl(dto.getImgUrl());
+		entidade.setPreco(dto.getPreco());
+		
+		entidade.getCategorias().clear();
+		for (CategoriaDTO catDto : dto.getCategorias()) {
+			Categoria categoria = categoriaRepositorio.getReferenceById(catDto.getId());
+			entidade.getCategorias().add(categoria);
+		}
+		
 	}
 	
 }
