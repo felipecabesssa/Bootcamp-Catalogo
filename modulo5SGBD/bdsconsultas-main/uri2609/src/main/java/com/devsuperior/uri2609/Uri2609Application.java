16dc8d8a0982ea6acddcba2f8bf2d2b4bc0ea4a7
felipecabesssa <felipesoares100@hotmail.com>
2022-09-07 17:52:17 -0300

    uri2609 - SELECT com INNER JOIN + GROUP BY e SUM (soma)

--- modulo5SGBD/bdsconsultas-main/uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ modulo5SGBD/bdsconsultas-main/uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,10 +1,15 @@
 package com.devsuperior.uri2609;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import com.devsuperior.uri2609.repositories.CategoryRepository;
 
 @SpringBootApplication
@@ -19,6 +24,21 @@ public class Uri2609Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		List<CategorySumProjection> lista = repository.buscaNativa();
+		List<CategorySumDTO> resultado1 = lista.stream().map(x -> new CategorySumDTO(x)).collect(Collectors.toList());
+		
+		System.out.println("\n*** RESULTADO SQL RAIZ (Natvive Query): ");
+		for (CategorySumDTO obj : resultado1) {
+			System.out.println(obj);
+		}
+		System.out.println("\n\n");
 
+		List<CategorySumDTO> resultado2 = repository.buscaJPQL();
+		
+		System.out.println("\n*** RESULTADO JPQL: ");
+		for (CategorySumDTO obj : resultado2) {
+			System.out.println(obj);
+		}
 	}
 }
